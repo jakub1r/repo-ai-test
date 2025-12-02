d56799d5ce7499894b04c081be9c7a54429550e4
jakub1r <your_email@example.com>
2025-12-02 21:42:47 +0100

    Add /android folder to repo

---
+++ android/app/src/main/java/com/jakub12345/fasttflite/SimpleMathPackage.kt
@@ -0,0 +1,19 @@
+package com.jakub12345.fasttflite
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class SimpleMathPackage : ReactPackage {
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(SimpleMathModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+
+    
+}
